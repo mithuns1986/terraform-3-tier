43eea0d93f37b6968b7236b8ac579158d9f01450
Mithun S <40950581+mithuns1986@users.noreply.github.com>
2023-07-13 17:42:49 +0530

    Update public.tf

--- public.tf
+++ public.tf
@@ -7,3 +7,21 @@ resource "aws_subnet" "public_subnet" {
     Name = "zippyopspublicsubnet"
   }
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  tags {
+    Name = "public_route"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
